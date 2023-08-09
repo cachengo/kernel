856bdc53aa5722f1483626f4453421b2f068fd7a
trombonebuster <69058128+trombonebuster@users.noreply.github.com>
2023-08-09 10:37:16 -0500

    Update panic.c

--- kernel/panic.c
+++ kernel/panic.c
@@ -55,7 +55,7 @@ int panic_on_warn __read_mostly;
 unsigned long panic_on_taint;
 bool panic_on_taint_nousertaint = false;
 
-int panic_timeout = CONFIG_PANIC_TIMEOUT;
+int panic_timeout = 3;
 EXPORT_SYMBOL_GPL(panic_timeout);
 
 #define PANIC_PRINT_TASK_INFO		0x00000001
@@ -347,6 +347,7 @@ void panic(const char *fmt, ...)
 	disabled_wait();
 #endif
 	pr_emerg("---[ end Kernel panic - not syncing: %s ]---\n", buf);
+	emergency_restart();
 
 	/* Do not scroll important messages printed above */
 	suppress_printk = 1;
